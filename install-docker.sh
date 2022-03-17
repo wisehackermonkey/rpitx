1d7a301e33ec00939c7a00611e82ce4870850a28
Oran C <oranbusiness@gmail.com>
2022-03-17 12:02:34 -0700

    removed asking for yes

--- install-docker.sh
+++ install-docker.sh
@@ -33,20 +33,20 @@ make install
 cd .. || exit
 
 printf "\n\n"
-printf "In order to run properly, rpitx need to modify /boot/config.txt. Are you sure (y/n) "
-read -r CONT
+printf "In order to run properly, rpitx need to modify /boot/config.txt. "
+# read -r CONT
 
-if [ "$CONT" = "y" ]; then
-  echo "Set GPU to 250Mhz in order to be stable"
-   LINE='gpu_freq=250'
-   FILE='/boot/config.txt'
-   grep -qF "$LINE" "$FILE"  || echo "$LINE" | tee --append "$FILE"
-   #PI4
-   LINE='force_turbo=1'
-   grep -qF "$LINE" "$FILE"  || echo "$LINE" | tee --append "$FILE"
-   echo "Installation completed !"
-else
-  echo "Warning : Rpitx should be instable and stop from transmitting !";
-fi
+# if [ "$CONT" = "y" ]; then#
+echo "Set GPU to 250Mhz in order to be stable"
+LINE='gpu_freq=250'
+FILE='/boot/config.txt'
+grep -qF "$LINE" "$FILE"  || echo "$LINE" | tee --append "$FILE"
+#PI4
+LINE='force_turbo=1'
+grep -qF "$LINE" "$FILE"  || echo "$LINE" | tee --append "$FILE"
+echo "Installation completed !"
+# else
+#   echo "Warning : Rpitx should be instable and stop from transmitting !";
+# fi
 
 
